aab3f7da40176413f54fd5b8c30e000773221881
{pierre-paul.quilichini} <{pierre-paul.quilichini@prehos.com}>
2025-07-13 17:20:38 -0400

    config v4

--- shopify_sdk/client.py
+++ shopify_sdk/client.py
@@ -2,8 +2,9 @@ from gql import Client
 from gql.transport.requests import RequestsHTTPTransport
 
 def init_graphql_client(shop_name, access_token):
+    url = f"https://{shop_name}/admin/api/2024-01/graphql.json"
     transport = RequestsHTTPTransport(
-        url = f"https://{shop_name}/admin/api/2024-01/graphql.json"
+        url=url,
         headers={
             "Content-Type": "application/json",
             "X-Shopify-Access-Token": access_token,
